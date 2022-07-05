b9cacc65362d6ed50f8d144f1d78b45b6af69588
marianntoots <mariann.toots@gmail.com>
2022-07-05 21:41:58 +0300

    commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// Küsimuse number, küsimus ja vastused
+let questions = [
+    {
+    numb: 1,
+    question: "ASCII on?",
+    answer: "Graafilise disaini tehnika",
+    options: [
+      "Programeerimiskeel",
+      "Graafilise disaini tehnika",
+      "Python-i üks raamistikest",
+      "Graafiline kujund"
+    ]
+  },
+    {
+    numb: 2,
+    question: "IT projektijuhi ülesanded on?",
+    answer: "Ärivajaduste analüüsimine",
+    options: [
+      "Projekti tarkvara arendus",
+      "Testilugude loomine",
+      "Ärivajaduste analüüsimine",
+      "Vastuvõtu testimine"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Mida väljendab KPI?",
+    answer: "Tulemuslikkuse võtmenäitaja",
+    options: [
+      "Võtmeklientide mugavuslahendus",
+      "Edukalt läbitud testide osakaal",
+      "Kiire Projekti Initsiatiiv",
+      "Tulemuslikkuse võtmenäitaja"
+    ]
+  },
+    {
+    numb: 4,
+    question: "PNG- formaadis pilt on?",
+    answer: "Kadudeta pakitud",
+    options: [
+      "Kadudega pakitud",
+      "Pakitud ZIP-failivormingusse",
+      "Kadudeta pakitud",
+      "JPG formaadi alamliik"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mis on NVRAM?",
+    answer: "Non-volatile random access Memory",
+    options: [
+      "Arvuti põhimälu",
+      "solid state drive",
+      "Non-volatile random access Memory",
+      "Dünaamiline muutmälu"
+    ]
+  },
+];
